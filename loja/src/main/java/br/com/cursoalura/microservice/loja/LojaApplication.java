45b49793984fe6dc4b1b88cbbca46c0d6751dce5
Renato Borges <renatoborges0501@gmail.com>
2020-04-08 00:35:33 -0300

    criando micro servico de fornecedor, compra e registrando no eureka server

--- loja/src/main/java/br/com/cursoalura/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/cursoalura/microservice/loja/LojaApplication.java
@@ -2,12 +2,19 @@ package br.com.cursoalura.microservice.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LojaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
